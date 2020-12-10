8dc0dbb1767dcb1dd64077ea168c70a4c9fcdc91
Arturo Jerez <xerezeno33@gmail.com>
2020-12-10 20:24:15 +0100

    [UPDATE] Visualización de las publicaciones

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,12 +3,14 @@
 // Cargar Librerias
 var express = require('express');
 var UserController = require('../controllers/user');
+var HobbieController = require('../controllers/hobbie');
 
 var api = express.Router();
 var md_auth = require('../middlewares/auth');
 var multiparty = require('connect-multiparty');
 var md_upload = multiparty({uploadDir: './uploads/users'});
 
+// USUARIO
 api.post('/register', UserController.save_user);
 api.post('/login', UserController.login_user);
 api.get('/user/:id', md_auth.ensureAuth, UserController.get_user);
@@ -17,5 +19,8 @@ api.put('/update-user/:id', md_auth.ensureAuth, UserController.update_user);
 api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.upload_image);
 api.get('/get-image-user/:image_file', UserController.get_image_file);
 api.get('/counters/:id?', md_auth.ensureAuth, UserController.get_counters);
+
+// HOBBIES DE USUARIO
+api.post('/save-hobbies', md_auth.ensureAuth, HobbieController.save_hobbies_user);
 
 module.exports = api;
